c4c147c37ddc1135ce74fa2fe535327254aeb5ee
ingole8888 <ingole8888@gmail.com>
2022-06-14 18:18:56 +0530

    sign up and login complete

---
+++ src/account.js
@@ -0,0 +1,71 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
+
+import { getDatabase,set,ref } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-database.js";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAR5zqqCggiYONEPLnnBXwFfc7eq_yyO18",
+    authDomain: "signup-6b16b.firebaseapp.com",
+    databaseURL: "https://signup-6b16b-default-rtdb.firebaseio.com",
+    projectId: "signup-6b16b",
+    storageBucket: "signup-6b16b.appspot.com",
+    messagingSenderId: "980295878149",
+    appId: "1:980295878149:web:e5231bec8ab6986e6d3e6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const database = getDatabase(app);
+
+
+document.getElementById("signup_form").addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const email = document.getElementById("form-email").value;
+    const password = document.getElementById("password").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+            set(ref(database, 'users/' + user.uid), {
+                email: email,
+                password: password,
+                
+            })
+                .then(() => {
+                    // Data saved successfully!
+                    console.log("created")
+                    alert("account created successfully")
+                    window.location.href = "./login.html"
+                })
+                .catch((error) => {
+                    // The write failed...
+                    alert(error)
+                });
+
+
+
+
+
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+            console.log(errorMessage)
+            alert("Error:" + errorCode)
+        });
+
+
+})
